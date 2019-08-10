cbc40bf77a8b01ff3770258e93072f91b9b4c9e1
sHesl <sHesl@users.noreply.github.com>
2019-08-10 17:36:03 +0100

    44,45,46

--- set6/dsa.go
+++ set6/dsa.go
@@ -7,12 +7,15 @@ import (
 	"math/big"
 )
 
-var dsaP, dsaQ, dsaG *big.Int
+var dsaP, dsaQ, dsaG, bigZero, bigOne *big.Int
 
 func init() {
 	dsaP, _ = new(big.Int).SetString("800000000000000089e1855218a0e7dac38136ffafa72eda7859f2171e25e65eac698c1702578b07dc2a1076da241c76c62d374d8389ea5aeffd3226a0530cc565f3bf6b50929139ebeac04f48c3c84afb796d61e5a4f9a8fda812ab59494232c7d2b4deb50aa18ee9e132bfa85ac4374d7f9091abc3d015efc871a584471bb1", 16)
 	dsaQ, _ = new(big.Int).SetString("f4f47f05794b256174bba6e9b396a7707e563c5b", 16)
 	dsaG, _ = new(big.Int).SetString("5958c9d3898b224b12672c0b98e06c60df923cb8bc999d119458fef538b8fa4046c8db53039db620c094c9fa077ef389b5322a559946a71903f990f1f7e0e025e2d7f7cf494aff1a0470f5b64c36b625a097f1651fe775323556fe00b3608c887892878480e99041be601a62166ca6894bdd41a7054ec89f756ba9fc95302291", 16)
+
+	bigZero = big.NewInt(0)
+	bigOne = big.NewInt(1)
 }
 
 type DSAKey struct {
@@ -107,4 +110,103 @@ func BruteForceViaWeakK(m []byte, r, sig, y *big.Int) *big.Int {
 	}
 
 	return nil
+}
+
+// CrackPrivKeyFromReusedK takes two messages that had a reused K and determines the priv component via math.
+//     (H(m1) - H(m2))
+// k = --------------- mod q
+//        (s1 - s2)
+func CrackPrivKeyFromReusedK(m1, m2 []byte, s1, s2, r *big.Int) *big.Int {
+	h1, h2 := sha1.Sum(m1), sha1.Sum(m2)
+	hm1, hm2 := new(big.Int).SetBytes(h1[:]), new(big.Int).SetBytes(h2[:])
+
+	m, mq := new(big.Int), new(big.Int)
+	m.Sub(hm1, hm2)
+	mq.Mod(m, dsaQ)
+
+	s, sq, sqInv := new(big.Int), new(big.Int), new(big.Int)
+	s.Sub(s1, s2)
+	sq.Mod(s, dsaQ)
+	sqInv.ModInverse(sq, dsaQ)
+
+	k := new(big.Int)
+	k.Mul(sqInv, mq).Mod(k, dsaQ)
+
+	rInv := new(big.Int).ModInverse(r, dsaQ)
+
+	// We only need to solve for one message, let's just use s1
+	x := new(big.Int).Mul(s1, k)
+	x.Mod(x, dsaQ)
+	x.Sub(x, hm1)
+	x.Mul(x, rInv)
+	x.Mod(x, dsaQ)
+
+	return x
+}
+
+func (key *DSAKey) SignWithCustomParams(m []byte, p, q, g *big.Int) (*big.Int, *big.Int) {
+	k, _ := rand.Int(rand.Reader, q)
+
+	// r = (g^^k mod p) mod q
+	gkp := new(big.Int).Exp(g, k, p)
+	r := new(big.Int).Mod(gkp, q)
+
+	// s = (k^^-1 (H(m)+xr)) mod q
+	h := sha256.Sum256(m)
+	hm := new(big.Int).SetBytes(h[:])
+
+	xr := new(big.Int).Mul(key.Priv, r)
+	hxr := new(big.Int).Add(hm, xr)
+
+	kInv := new(big.Int).ModInverse(k, q)
+	kInvHMXR := new(big.Int).Mul(kInv, hxr)
+
+	s := new(big.Int).Mod(kInvHMXR, q)
+
+	return r, s
+}
+
+func (key *DSAKey) VerifyWithCustomParams(m []byte, r, s, p, q, g *big.Int) bool {
+	w := new(big.Int).ModInverse(s, q)
+
+	h := sha256.Sum256(m)
+	hm := new(big.Int).SetBytes(h[:])
+
+	u1 := new(big.Int).Mul(hm, w)
+	u1.Mod(u1, q)
+
+	u2 := new(big.Int).Mul(r, w)
+	u2.Mod(u2, q)
+
+	gu1 := new(big.Int).Exp(g, u1, p)
+	yu2 := new(big.Int).Exp(key.Pub, u2, p)
+	gu1yu2 := new(big.Int).Mul(gu1, yu2)
+	v := new(big.Int).Mod(gu1yu2, p)
+	v.Mod(v, q)
+
+	return v.Cmp(r) == 0
+}
+
+func (key *DSAKey) MagicSignature() (*big.Int, *big.Int) {
+	z, _ := rand.Int(rand.Reader, dsaQ)
+
+	// r = ((y**z) % p) % q
+	yz := new(big.Int).Exp(key.Pub, z, dsaP)
+
+	r := new(big.Int)
+	r.Mod(yz, dsaQ)
+
+	//       r
+	// s =  --- % q
+	//       z
+	// aka
+	//
+	// s =  ((r % q) * modInv(z, q)) % q
+	//
+	s := new(big.Int)
+	rq := new(big.Int).Mod(r, dsaQ)
+	zq := new(big.Int).ModInverse(z, dsaQ)
+	s.Mul(rq, zq).Mod(s, dsaQ)
+
+	return r, s
 }
--- set6/set6_test.go
+++ set6/set6_test.go
@@ -7,6 +7,7 @@ import (
 	"crypto/rsa"
 	"crypto/sha1"
 	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
 	"math/big"
 	"testing"
@@ -32,7 +33,7 @@ func Test_Challenge41_RSAUnpaddedMessageRecovery(t *testing.T) {
 	pubKey := privKey.PublicKey
 	c1 := new(big.Int).SetBytes(ciphertext)
 
-	// se = S**Em od N
+	// se = S**E mod N
 	// C2 = (se * C1) mod N
 	s := big.NewInt(25) // s can be anything > 1 mod N
 	se := new(big.Int).Exp(s, big.NewInt(int64(pubKey.E)), pubKey.N)
@@ -128,6 +129,190 @@ So be friendly, a matter of life and death, just like a etch-a-sketch
 	xDigest := fmt.Sprintf("%x", sha1.Sum([]byte(xHex)))
 
 	if xDigest == "0954edd5e0afe5542a4adf012611a91912a3ec16" {
-		fmt.Printf("Challenge 43: Cracked a DSA private component via weak K! x = %X", x.Bytes())
+		fmt.Printf("Challenge 43: Cracked a DSA private component via weak K! x = %X\n", x.Bytes())
 	}
+}
+
+func Test_Challenge44_DSANonceReuse(t *testing.T) {
+	type signedMessage struct {
+		msg []byte
+		r   string
+		s   string
+		m   []byte
+	}
+
+	messages := []signedMessage{
+		{
+			msg: []byte("Listen for me, you better listen for me now. "),
+			s:   "1267396447369736888040262262183731677867615804316",
+			r:   "1105520928110492191417703162650245113664610474875",
+			m:   []byte("a4db3de27e2db3e5ef085ced2bced91b82e0df19"),
+		}, {
+			msg: []byte("Listen for me, you better listen for me now. "),
+			s:   "29097472083055673620219739525237952924429516683",
+			r:   "51241962016175933742870323080382366896234169532",
+			m:   []byte("a4db3de27e2db3e5ef085ced2bced91b82e0df19"),
+		}, {
+			msg: []byte("When me rockin' the microphone me rock on steady, "),
+			s:   "277954141006005142760672187124679727147013405915",
+			r:   "228998983350752111397582948403934722619745721541",
+			m:   []byte("21194f72fe39a80c9c20689b8cf6ce9b0e7e52d4"),
+		}, {
+			msg: []byte("Yes a Daddy me Snow me are de article dan. "),
+			s:   "1013310051748123261520038320957902085950122277350",
+			r:   "1099349585689717635654222811555852075108857446485",
+			m:   []byte("1d7aaaa05d2dee2f7dabdc6fa70b6ddab9c051c5"),
+		}, {
+			msg: []byte("But in a in an' a out de dance em "),
+			s:   "203941148183364719753516612269608665183595279549",
+			r:   "425320991325990345751346113277224109611205133736",
+			m:   []byte("6bc188db6e9e6c7d796f7fdd7fa411776d7a9ff"),
+		}, {
+			msg: []byte("Aye say where you come from a, "),
+			s:   "502033987625712840101435170279955665681605114553",
+			r:   "486260321619055468276539425880393574698069264007",
+			m:   []byte("5ff4d4e8be2f8aae8a5bfaabf7408bd7628f43c9"),
+		}, {
+			msg: []byte("People em say ya come from Jamaica, "),
+			s:   "1133410958677785175751131958546453870649059955513",
+			r:   "537050122560927032962561247064393639163940220795",
+			m:   []byte("7d9abd18bbecdaa93650ecc4da1b9fcae911412"),
+		}, {
+			msg: []byte("But me born an' raised in the ghetto that I want yas to know, "),
+			s:   "559339368782867010304266546527989050544914568162",
+			r:   "826843595826780327326695197394862356805575316699",
+			m:   []byte("88b9e184393408b133efef59fcef85576d69e249"),
+		}, {
+			msg: []byte("Pure black people mon is all I mon know. "),
+			s:   "1021643638653719618255840562522049391608552714967",
+			r:   "1105520928110492191417703162650245113664610474875",
+			m:   []byte("d22804c4899b522b23eda34d2137cd8cc22b9ce8"),
+		}, {
+			msg: []byte("Yeah me shoes a an tear up an' now me toes is a show a "),
+			s:   "506591325247687166499867321330657300306462367256",
+			r:   "51241962016175933742870323080382366896234169532",
+			m:   []byte("bc7ec371d951977cba10381da08fe934dea80314"),
+		}, {
+			msg: []byte("Where me a born in are de one Toronto, so "),
+			s:   "458429062067186207052865988429747640462282138703",
+			r:   "228998983350752111397582948403934722619745721541",
+			m:   []byte("d6340bfcda59b6b75b59ca634813d572de800e8f"),
+		},
+	}
+
+	y := new(big.Int)
+	y.SetString("2d026f4bf30195ede3a088da85e398ef869611d0f68f0713d51c9c1a3a26c95105d915e2d8cdf26d056b86b8a7b85519b1c23cc3ecdc6062650462e3063bd179c2a6581519f674a61f1d89a1fff27171ebc1b93d4dc57bceb7ae2430f98a6a4d83d8279ee65d71c1203d2c96d65ebbf7cce9d32971c3de5084cce04a2e147821", 16)
+
+	for i, msg1 := range messages {
+		for ii, msg2 := range messages {
+			if i == ii {
+				continue
+			}
+
+			// r = (g^^k mod p) mod q
+			// so if k (the nonce) is reused across two messages, r will also be the same!
+			if msg1.r == msg2.r {
+				s1, s2 := new(big.Int), new(big.Int)
+				s1.SetString(msg1.s, 10)
+				s2.SetString(msg2.s, 10)
+
+				r := new(big.Int)
+				r.SetString(msg1.r, 10)
+				// s = (k^^-1 * (H(m)+xr)) mod q
+				// So in the nonce reuse scenarios, x,r,k and q are all identical between signings. If we have the
+				// original message as well, we can compute H(m), so we have every element except x. We just need to
+				// factor out these values (and calculate the distance between the two hashes) to reveal x.
+				x := CrackPrivKeyFromReusedK(msg1.msg, msg2.msg, s1, s2, r)
+
+				xHex := fmt.Sprintf("%x", x)
+				xDigest := fmt.Sprintf("%x", sha1.Sum([]byte(xHex)))
+
+				if xDigest == "ca8f6f7c66fa362d40760d135b763eb8527d3d52" {
+					fmt.Printf("Challenge 44: Cracked a DSA private component via reused K! x = %X\n", x.Bytes())
+					return
+				}
+			}
+		}
+	}
+}
+
+func Test_Challenge45_DSADodgeyParams(t *testing.T) {
+	key := NewDSAKey()
+
+	r, s := key.SignWithCustomParams([]byte(`uh oh, g is zero!`), dsaP, dsaQ, bigZero) // r == 0
+
+	anyMessageValidates := key.VerifyWithCustomParams([]byte(`message has changed!`), r, s, dsaP, dsaQ, bigZero)
+
+	if !anyMessageValidates {
+		t.Fatalf("when g=0, all signatures should pass validation")
+	}
+
+	msg := []byte(`this message can be verified without x because of magic g!`)
+
+	r, s = key.MagicSignature()
+
+	magicG := new(big.Int).Add(dsaP, bigOne) // this is the magic number that enables magic signatures!
+	magicSignatureVerified := key.VerifyWithCustomParams(msg, r, s, dsaP, dsaQ, magicG)
+
+	if magicSignatureVerified {
+		fmt.Printf("Challenge 45: Used magic G to DSA sign message without x!\n")
+	}
+}
+
+func Test_Challenge46_RSAParityOracle(t *testing.T) {
+	priv, _ := rsa.GenerateKey(rand.Reader, 1024)
+
+	oddOracle := func(x *big.Int) bool {
+		plaintext := set5.RSADecrypt(*priv, x.Bytes())
+		y := new(big.Int)
+		y.SetBytes(plaintext)
+
+		mod2 := y.Mod(y, big.NewInt(2))
+
+		return mod2.Cmp(bigZero) != 0
+	}
+
+	secretB64 := "VGhhdCdzIHdoeSBJIGZvdW5kIHlvdSBkb24ndCBwbGF5IGFyb3VuZCB3aXRoIHRoZSBGdW5reSBDb2xkIE1lZGluYQ=="
+	secret, _ := base64.StdEncoding.DecodeString(secretB64)
+	s := set5.RSAEncrypt(priv.PublicKey, secret)
+	c := new(big.Int).SetBytes(s)
+
+	e := big.NewInt(int64(priv.PublicKey.E))
+	factor := big.NewInt(2) // start from 2, but we will x2 this every round
+	bigTwo := big.NewInt(2) // keep a big 2 constant for x2ing our factor each round
+
+	// our plaintext exists somewhere within the bound [0, N]
+	upperBound := priv.N
+	lowerBound := big.NewInt(0)
+
+	bitLen := len(c.Bytes()) * 8 // We need to do this once per bit of the message
+
+	for i := 0; i < bitLen; i++ {
+		// N is a prime (and therefore odd), so we have to multiply our ciphertext by a multiple of 2, otherwise
+		// it would always be false! Since our parity oracle decrypts the contents, we can't just do ciphertext*f,
+		// we need to do c * enc(f). We can use the public key to encrypt the
+		enc2 := new(big.Int).Exp(factor, e, priv.PublicKey.N)
+		cf := new(big.Int).Mul(c, enc2) // original ciphertext * our factor
+
+		// Each round, we learn whether our cf/n wraps the modulus (even), or doesn't (odd).
+		// If it does wrap the modulus, the real plaintext lies below that value.
+		// If it doesn't, the real plaintext lies above that value.
+		// We can then narrow our total range down by either raising the lower bound (odd case)
+		// Or lowering the upper bound (even case).
+		newBound := new(big.Int).Add(upperBound, lowerBound)
+		newBound.Div(newBound, bigTwo)
+
+		if odd := oddOracle(cf); odd {
+			lowerBound = newBound
+		} else {
+			upperBound = newBound
+		}
+
+		// As we narrow our bounds, we need to increase the amount of time we multiply our ciphertext to force the
+		// ciphertext to fold over N more and more times (for example, folding over 3 times = odd, folding over 8
+		// times = even).
+		factor.Mul(factor, bigTwo)
+	}
+
+	fmt.Printf("Challenge 46: Cracked RSA encrypted ciphertext via parrity oracle. \n\tMessage: '%s'\n", lowerBound.Bytes())
 }
